ccb592b8ed815aa9ec57870a414d75b181171474
Robin Stahl Warelius <107325879+robinwarelius@users.noreply.github.com>
2023-09-19 15:12:24 +0200

    init

---
+++ Uppgift_01/Solution_Uppgift_01/API_Uppgift_01/Controllers/HomeController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API_Uppgift_01.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HomeController : ControllerBase
+    {
+        [HttpGet("ReturnSometing")]
+        public async Task <IActionResult> ReturnSomething()
+        {
+            return null;
+        } 
+    }
+}
