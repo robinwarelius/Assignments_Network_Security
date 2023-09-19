c34649cf6937b80b279f041d522917ccf2c811b4
Robin Stahl Warelius <107325879+robinwarelius@users.noreply.github.com>
2023-09-19 22:14:51 +0200

    uppgift 2

---
+++ Uppgift_02/Solution_Uppgift_02/Client_01/Program.cs
@@ -0,0 +1,24 @@
+﻿using System.Net.Sockets;
+using System.Net;
+using System.Text;
+
+
+int port = 12345;
+UdpClient udpClient = new UdpClient();
+udpClient.Client.Bind(new IPEndPoint(IPAddress.Any, port));
+
+Console.WriteLine("Waiting on messages...");
+
+// Starta tråd för att lyssna på inkommande meddelanden
+Thread receiveThread = new Thread(() =>
+{
+    while (true)
+    {
+        IPEndPoint remoteEndPoint = new IPEndPoint(IPAddress.Any, port);
+        byte[] data = udpClient.Receive(ref remoteEndPoint);
+        string message = Encoding.UTF8.GetString(data);
+        Console.WriteLine($"Recieved message from {remoteEndPoint}: {message}");
+    }
+});
+receiveThread.Start();
+
---
+++ Uppgift_02/Solution_Uppgift_02/Client_02/Program.cs
@@ -0,0 +1,17 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+int port = 12345;
+UdpClient udpClient = new UdpClient();
+
+// Send message
+while (true)
+{
+    Console.Write("Enter a message: ");
+    string messageToSend = Console.ReadLine();
+    byte[] data = Encoding.UTF8.GetBytes(messageToSend);
+    udpClient.Send(data, data.Length, "127.0.0.1", port);
+}
+
+
