adfce601a1f64ccc7e43d3af7f35b6b47440d92f
Robin Stahl Warelius <107325879+robinwarelius@users.noreply.github.com>
2023-09-20 08:42:07 +0200

    add comments

--- Uppgift_01/Solution_Uppgift_01/Client/Program.cs
+++ Uppgift_01/Solution_Uppgift_01/Client/Program.cs
@@ -5,18 +5,20 @@ using Client.Models;
 
 while (true)
 {
-    using var client = new TcpClient("localhost", 12345);
-    using var reader = new StreamReader(client.GetStream());
-    using var writer = new StreamWriter(client.GetStream());
+    using var client = new TcpClient("localhost", 12345);   // Skapa en TCP-klient och anslut till servern på localhost (min egen dator) och port 12345
+    using var reader = new StreamReader(client.GetStream());   // Skapa en läsare för att läsa inkommande data från servern
+    using var writer = new StreamWriter(client.GetStream());   // Skapa en skrivare för att skicka data till servern
 
-    Console.WriteLine("Enter a message to send to the server:");
-    var input = Console.ReadLine() ?? "No message";
-    var message = new Message { Content = input };
-    writer.WriteLine(JsonSerializer.Serialize(message));
-    writer.Flush();
+    Console.WriteLine("Enter a message to send to the server:"); 
 
-    var response = reader.ReadLine();
-    var responseMessage = JsonSerializer.Deserialize<Message>(response);
+    var input = Console.ReadLine() ?? "No message";   
+    var message = new Message { Content = input };   // Skapa ett Message-objekt med användarens meddelande
 
-    Console.WriteLine("Received: " + responseMessage!.Content);
+    writer.WriteLine(JsonSerializer.Serialize(message));   // Skicka meddelandet till servern som JSON
+    writer.Flush();   // Töm skrivbufferten
+
+    var response = reader.ReadLine();   // Läs serverns svar
+    var responseMessage = JsonSerializer.Deserialize<Message>(response);   // Deserialisera JSON-svaret till ett Message-objekt
+
+    Console.WriteLine("Received: " + responseMessage!.Content);   // Skriv ut serverns svar på konsolen
 }
--- Uppgift_01/Solution_Uppgift_01/Server/Program.cs
+++ Uppgift_01/Solution_Uppgift_01/Server/Program.cs
@@ -3,25 +3,25 @@ using System.Net;
 using System.Net.Sockets;
 using System.Text.Json;
 
-// Create listener
-var listener = new TcpListener(IPAddress.Any, 12345);
-listener.Start();
+// Skapa en lyssnare (listener) för att lyssna på inkommande anslutningar
+var listener = new TcpListener(IPAddress.Any, 12345); // Lyssnar på alla tillgängliga nätverksgränssnitt på port 12345
+listener.Start(); // Starta lyssnaren
 Console.WriteLine($"Server started on port 12345 | Date: {DateTime.Now.ToShortDateString()} | Time: {DateTime.Now.ToShortTimeString()}");
 
 while (true)
 {
-    var client = listener.AcceptTcpClient();
-    var reader = new StreamReader(client.GetStream());
-    var writer = new StreamWriter(client.GetStream());
+    var client = listener.AcceptTcpClient(); // Acceptera en inkommande TCP-anslutning från en klient
+    var reader = new StreamReader(client.GetStream()); // Skapa en läsare för att läsa inkommande data från klienten
+    var writer = new StreamWriter(client.GetStream()); // Skapa en skrivare för att skicka data till klienten
 
-    var json = reader.ReadLine();
-    var message = JsonSerializer.Deserialize<Message>(json);
+    var json = reader.ReadLine(); // Läs en rad med JSON-data från klienten
+    var message = JsonSerializer.Deserialize<Message>(json); // Deserialisera JSON-data till ett Message-objekt
 
     Console.WriteLine($"Received Message: {message!.Content}");
 
     message.Content = $" '{message.Content}' was received!";
-    writer.WriteLine(JsonSerializer.Serialize(message));
-    writer.Flush();
+    writer.WriteLine(JsonSerializer.Serialize(message)); // Skicka det ändrade meddelande tillbaka till klienten som JSON
+    writer.Flush(); // Töm skrivbufferten
 
-    client.Close();
+    client.Close(); // Stäng TCP-anslutningen med klienten
 }
